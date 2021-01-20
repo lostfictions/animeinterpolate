3eeb8e57d1f64176e7c072662fd45a86655ced68
lostfictions <lostfictions@users.noreply.github.com>
2021-01-19 20:37:18 -0500

    add server

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,4 @@
-import { createWriteStream } from "fs";
-import path from "path";
-
-// import axios from "axios";
+import Koa from "koa";
 import { createCanvas, Image } from "canvas";
 import GIFEncoder from "gif-encoder-2";
 
@@ -25,14 +22,10 @@ async function* getImages(seed: string) {
     const url = geturl(psi.toFixed(1), seed);
     if (lasturl === url) continue;
     lasturl = url;
-    console.log("adding", url);
+    // console.log("adding", url);
     yield getImage(url);
   }
 }
-
-// function lerpr(from: number, to: number, t: number): string {
-//   return (from * (1 - t) + to * t).toFixed(1);
-// }
 
 function rint(min: number, max: number): number {
   return Math.floor(Math.random() * (max - min)) + min;
@@ -40,30 +33,33 @@ function rint(min: number, max: number): number {
 
 const getSeed = () => rint(0, 50000).toString().padStart(4, "0");
 
-async function createGif({ seed = getSeed(), delay = 50 } = {}) {
-  const dest = path.join(__dirname, `../out/${seed}.gif`);
-  const writeStream = createWriteStream(dest);
-  writeStream.on("error", (e) => {
-    throw e;
-  });
+const app = new Koa();
+
+app.use(async ({ path, res, query }) => {
+  if (path !== "/") {
+    res.statusCode = 404;
+    return;
+  }
+
+  const { seed, delay } = query;
+
+  const padded = seed ? seed.toString().padStart(4, "0") : getSeed();
 
   const encoder = new GIFEncoder(512, 512, "neuquant");
-  encoder.createReadStream().pipe(writeStream);
+
+  encoder.createReadStream().pipe(res);
   encoder.start();
-  encoder.setDelay(delay);
+  encoder.setDelay(parseInt(delay) || 50);
 
   const canvas = createCanvas(512, 512);
   const ctx = canvas.getContext("2d");
 
-  for await (const i of getImages(seed)) {
+  for await (const i of getImages(padded)) {
     ctx.drawImage(i, 0, 0);
     encoder.addFrame(ctx);
   }
 
   encoder.finish();
-  // writeStream.close();
-}
-
-void createGif().then(() => {
-  console.log("done");
 });
+
+app.listen(3000);
